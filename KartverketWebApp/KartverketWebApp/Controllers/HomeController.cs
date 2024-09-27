0da84257974a2ebdff19e21ad6c66444639986ce
FredrikHusebruh <fredriksh@uia.no>
2024-09-27 10:51:58 +0200

    ny prototype

--- KartverketWebApp/KartverketWebApp/Controllers/HomeController.cs
+++ KartverketWebApp/KartverketWebApp/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using KartverketWebApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text.Json; // Make sure to include this for JSON serialization
 
 namespace KartverketWebApp.Controllers
 {
@@ -8,14 +9,11 @@ namespace KartverketWebApp.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        private static List<PositionModel> positions = new List<PositionModel>();
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
         }
 
         public IActionResult TakkRapport()
@@ -32,6 +30,26 @@ namespace KartverketWebApp.Controllers
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(PositionModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                positions.Add(model);
+
+                return View("CorrectionsOverview", positions);
+            }
+
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ KartverketWebApp/KartverketWebApp/Models/PositionModel.cs
@@ -0,0 +1,11 @@
+﻿namespace KartverketWebApp.Models
+{
+    public class PositionModel
+    {
+        public string Latitude { get; set; }
+
+        public string Longitude { get; set; }
+
+        public string Description { get; set; }
+    }
+}
