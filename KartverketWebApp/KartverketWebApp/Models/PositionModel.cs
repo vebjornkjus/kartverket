34255a4e6303a443e5af1aed9ca561bdf3daf5f6
FredrikHusebruh <fredriksh@uia.no>
2024-09-30 19:00:02 +0200

    lagt til ny forside og annet

--- KartverketWebApp/KartverketWebApp/Models/PositionModel.cs
+++ KartverketWebApp/KartverketWebApp/Models/PositionModel.cs
@@ -6,6 +6,13 @@
 
         public string Longitude { get; set; }
 
+        public string Brukernavn { get; set; }
+
+
         public string Description { get; set; }
+
+        public string Map_type { get; set; }
+
+        public string Rapport_type { get; set; }
     }
 }
