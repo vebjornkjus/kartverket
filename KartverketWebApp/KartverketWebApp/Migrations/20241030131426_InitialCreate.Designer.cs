86d452cefd59b3b546f4472c0e6c5908fd32301d
FredrikHusebruh <fredriksh@uia.no>
2024-10-31 08:51:02 +0100

    database test data

--- KartverketWebApp/KartverketWebApp/Migrations/20241030131426_InitialCreate.Designer.cs
+++ KartverketWebApp/KartverketWebApp/Migrations/20241030131426_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace KartverketWebApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20241029111904_InitialCreate")]
+    [Migration("20241030131426_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
