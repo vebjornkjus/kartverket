ef61ef444e40e7a303ed43a33a1fe62401e260fc
vebjornkjus <vebjornfk@uia.no>
2024-11-24 14:02:44 +0100

    Endringer i admincontroller

--- KartverketWebApp/KartverketWebApp/Controllers/AdminController.cs
+++ KartverketWebApp/KartverketWebApp/Controllers/AdminController.cs
@@ -13,17 +13,14 @@ namespace KartverketWebApp.Controllers
         private readonly ApplicationDbContext _context;
         private readonly IDbConnection _dbConnection;
 
-        // Constructor for å bruke DbContext
         public AdminController(ApplicationDbContext context, IDbConnection dbConnection)
         {
             _context = context;
             _dbConnection = dbConnection;
         }
 
-        // Metode for å vise BrukerOversikt med filtrering
         public async Task<IActionResult> BrukerOversikt(string filter = null)
         {
-            // Hvis filter er null, hent alle brukerne
             var brukerOversiktQuery = _context.Bruker
                 .Join(_context.Person,
                     b => b.BrukerId,
@@ -37,7 +34,6 @@ namespace KartverketWebApp.Controllers
                         Etternavn = p.Etternavn
                     });
 
-            // Hvis filter er satt, filtrer på BrukerType
             if (!string.IsNullOrEmpty(filter))
             {
                 brukerOversiktQuery = brukerOversiktQuery.Where(b => b.BrukerType == filter);
@@ -48,35 +44,30 @@ namespace KartverketWebApp.Controllers
             return View("~/Views/Home/Admin/BrukerOversikt.cshtml", brukerOversikt);
         }
 
-        // Metode for å slette en bruker
         [HttpPost]
-        public IActionResult SlettBruker(int id)
+        public async Task<IActionResult> SlettBruker(int id)
         {
-            var bruker = _context.Bruker.FirstOrDefault(b => b.BrukerId == id);
-            var person = _context.Person.FirstOrDefault(p => p.BrukerId == id);
+            var bruker = await _context.Bruker.FirstOrDefaultAsync(b => b.BrukerId == id);
+            var person = await _context.Person.FirstOrDefaultAsync(p => p.BrukerId == id);
 
             if (bruker != null)
             {
-                // Slett bruker og person
                 _context.Bruker.Remove(bruker);
                 if (person != null)
                 {
                     _context.Person.Remove(person);
                 }
 
-                _context.SaveChanges();
+                await _context.SaveChangesAsync();
             }
 
-            // Etter sletting, send brukeren tilbake til BrukerOversikt-siden
             return RedirectToAction("BrukerOversikt");
         }
 
-
-        // Metode for å vise redigeringsskjema
         [HttpGet]
-        public IActionResult RedigerBruker(int id)
+        public async Task<IActionResult> RedigerBruker(int id)
         {
-            var bruker = _context.Bruker
+            var bruker = await _context.Bruker
                 .Join(_context.Person,
                       b => b.BrukerId,
                       p => p.BrukerId,
@@ -88,7 +79,7 @@ namespace KartverketWebApp.Controllers
                           Fornavn = p.Fornavn,
                           Etternavn = p.Etternavn
                       })
-                .FirstOrDefault(b => b.BrukerId == id);
+                .FirstOrDefaultAsync(b => b.BrukerId == id);
 
             if (bruker == null)
             {
@@ -98,22 +89,20 @@ namespace KartverketWebApp.Controllers
             return View("~/Views/Home/Admin/RedigerBruker.cshtml", bruker);
         }
 
-        // Metode for å oppdatere brukerdata
         [HttpPost]
-        public IActionResult RedigerBruker(BrukerOversiktViewModel model)
+        public async Task<IActionResult> RedigerBruker(BrukerOversiktViewModel model)
         {
-            var bruker = _context.Bruker.FirstOrDefault(b => b.BrukerId == model.BrukerId);
-            var person = _context.Person.FirstOrDefault(p => p.BrukerId == model.BrukerId);
+            var bruker = await _context.Bruker.FirstOrDefaultAsync(b => b.BrukerId == model.BrukerId);
+            var person = await _context.Person.FirstOrDefaultAsync(p => p.BrukerId == model.BrukerId);
 
             if (bruker != null && person != null)
             {
-                // Oppdater brukerdata
                 bruker.Email = model.Email;
                 bruker.BrukerType = model.BrukerType;
                 person.Fornavn = model.Fornavn;
                 person.Etternavn = model.Etternavn;
 
-                _context.SaveChanges();
+                await _context.SaveChangesAsync();
                 return RedirectToAction("BrukerOversikt");
             }
 
